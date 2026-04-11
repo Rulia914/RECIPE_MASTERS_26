5a33c91a250f4283be1970920b5fdf5898784d36
Rulia914 <falconejulie@gmail.com>
2026-04-11 11:34:17 +0200

    création des dossiers de base

---
+++ app/config/params.php
@@ -0,0 +1,16 @@
+<?php
+
+
+// 1. Intitialiser les zones dynamiques: $title et $content
+$title = "";
+$content = "Aucun contenu ne correspond à votre requête.";
+
+
+// 2. Définir les constantes de connexion 
+//    DB_HOST, DB_NAME, DB_USER, DB_PASSWORD
+
+
+define("DB_HOST", "localhost:3307");
+define("DB_NAME", "recipe_master_26");
+define("DB_USER", "root");
+define("DB_PASSWORD", "");
